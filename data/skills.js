2f2a1ea9aa8d93399138459bcda1cd3e4cbd3f84
jessikim <jessica.kim17@gmail.com>
2022-09-20 21:06:33 -0500

    create unordered list in views and import skills from data to router

--- data/skills.js
+++ data/skills.js
@@ -1,10 +1,10 @@
 const skills = [
-  {name: 'HTML', week: 2, unit: 1, keyword: 'markup language', covered: true},
-  {name: 'CSS', week: 2, unit: 1, keyword: 'style sheet language', covered: true},
-  {name: 'JavaScript', week: 1, unit: 1, keyword: 'programming language', covered: true},
   {name: 'Terminal', week: 1, unit: 1, keyword: 'command-line interface', covered: true},
   {name: 'Git', week: 1, unit: 1, keyword: 'version control system', covered: true},
   {name: 'GitHub', week: 1, unit: 1, keyword: 'social network', covered: true},
+  {name: 'HTML', week: 2, unit: 1, keyword: 'markup language', covered: true},
+  {name: 'CSS', week: 2, unit: 1, keyword: 'style sheet language', covered: true},
+  {name: 'JavaScript', week: 1, unit: 1, keyword: 'programming language', covered: true},
   {name: 'Node', week: 5, unit: 2, keyword: 'runtime environment', covered: true},
   {name: 'Express', week: 5, unit: 2, keyword: 'web framework', covered: true},
   {name: 'MongoDB', week: 5, unit: 2, keyword: 'database system', covered: true},
