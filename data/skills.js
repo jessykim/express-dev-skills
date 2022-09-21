cb344d9a5ef0116fabf7fca2405e744f0475855b
jessikim <jessica.kim17@gmail.com>
2022-09-20 20:52:41 -0500

    add data to skills.js file

---
+++ data/skills.js
@@ -0,0 +1,22 @@
+const skills = [
+  {name: 'HTML', week: 2, unit: 1, keyword: 'markup language', covered: true},
+  {name: 'CSS', week: 2, unit: 1, keyword: 'style sheet language', covered: true},
+  {name: 'JavaScript', week: 1, unit: 1, keyword: 'programming language', covered: true},
+  {name: 'Terminal', week: 1, unit: 1, keyword: 'command-line interface', covered: true},
+  {name: 'Git', week: 1, unit: 1, keyword: 'version control system', covered: true},
+  {name: 'GitHub', week: 1, unit: 1, keyword: 'social network', covered: true},
+  {name: 'Node', week: 5, unit: 2, keyword: 'runtime environment', covered: true},
+  {name: 'Express', week: 5, unit: 2, keyword: 'web framework', covered: true},
+  {name: 'MongoDB', week: 5, unit: 2, keyword: 'database system', covered: true},
+  {name: 'Mongoose', week: 5, unit: 2, keyword: 'object document mapper', covered: true},
+  {name: 'React', week: 8, unit: 3, keyword: 'front-end library', covered: false},
+  {name: 'Python', week: null, unit: 3, keyword: 'programming language', covered: false},
+  {name: 'PostgreSQL', week: null, unit: 4, keyword: 'database system', covered: false},
+  {name: 'Django', week: null, unit: 4, keyword: 'web framework', covered: false},
+  {name: 'jQuery', week: null, unit: 4, keyword: 'JS library', covered: false},
+  {name: 'Flask', week: null, unit: 4, keyword: 'micro web framework', covered: false},
+]
+
+export {
+  skills
+}
