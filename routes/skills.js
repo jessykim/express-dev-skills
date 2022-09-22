bb4938117d2e97a4ffb63d87983c0a07d1703520
jessikim <jessica.kim17@gmail.com>
2022-09-22 14:36:23 -0500

    create a link for new skills

--- routes/skills.js
+++ routes/skills.js
@@ -6,8 +6,10 @@ const router = Router()
 /* GET users listing. */
 router.get('/', skillsCtrl.index)
 
+// GET - localhost:3000/skills/new
 router.get('/new', skillsCtrl.new)
 
+// POST - localhost:3000/skills
 router.post('/', skillsCtrl.create)
 
 
